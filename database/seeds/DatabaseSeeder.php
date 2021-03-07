bb2f454731758d4bbd3faf2dbaf1f9dce4059b98
AbdelrahmanBl2020 <61190351+AbdelrahmanBl2020@users.noreply.github.com>
2021-03-07 12:14:19 +0200

    ne

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(createAdminSeeder::class);
         // $this->call(packagesSeederTable::class);
         $this->call(settingsSeeder::class);
-        // $this->call(daysSeederTable::class);
+        $this->call(daysSeederTable::class);
         // $this->call(exam_requestsSeederTable::class);
         $this->call(teacherSeeder::class);
         $this->call(appointmentSeeder::class);
